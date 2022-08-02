88825b7a0dc05e3f41cd4dd49c3198b0ec31793d
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-08-02 15:31:28 +0200

    Fix errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
+const Square1 = require('./5-square.js');
 
-// Print function with custom characters to represent the Square
-
-const OldSquare = require('./5-square');
-
-module.exports = class Square extends OldSquare {
-  constructor (size) {
-    super(size, size);
-  }
-
-  double () {
-    super.double();
-  }
-
-  charPrint (c = 'X') {
-    super.print(c);
+module.exports = class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
+    }
   }
 };
